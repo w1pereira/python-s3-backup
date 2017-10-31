3f5486bb8b634fb10dda6770c35af39d5c327960
William Pereira <william@williampereira.me>
2017-10-31 09:26:09 -0200

    refactoring and organizing

--- backup.py
+++ backup.py
@@ -1,11 +1,11 @@
-"""This script zip backup folders (according to an regex pattern)
-    and send it to Amazon S3 service."""
+"""This script zip folders (according to an regex pattern)
+    and upload to Amazon S3 service."""
 
 import os
 import re
 import zipfile
 
-PATH = '.'
+# PATH = '.'
 
 def zipdir(path, ziph):
     """Zip entire directory."""
@@ -13,13 +13,18 @@ def zipdir(path, ziph):
         for file in files:
             ziph.write(os.path.join(root, file))
 
-for directories in os.walk(PATH):
-    for d in directories[1]:
-        # Folder name examples: Backup_Relatorios_2110_2017_00_00, Backup_Imagens_2110_2017_00_00
-        folder_name = re.search('_(.*)_(\\d{2})(\\d{2})_(\\d{4})_(\\d{2})_(\\d{2})', d)
-        if folder_name is not None:
-            filename = ('' if PATH == '.' else PATH) + d + '.zip'
-            zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-            zipdir(d, zipf)
-            zipf.close()
-    break
+def backup(path, pattern):
+    """Backup folders based on given path and regex pattern."""
+    for directories in os.walk(path):
+        for directory in directories[1]:
+            folder_name = re.search(pattern, directory)
+            if folder_name is not None:
+                filename = ('' if path == '.' else path) + directory + '.zip'
+                zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+                zipdir(directory, zipf)
+                zipf.close()
+        break
+
+# Folder name examples: Backup_Relatorios_2110_2017_00_00,
+# Backup_Imagens_2110_2017_00_00
+# backup(PATH, '_(.*)_(\\d{2})(\\d{2})_(\\d{4})_(\\d{2})_(\\d{2})')
